8db9f2ce3e944abe267b18dc825fdb410e6d3f93
Yurii Svystun <svystun@gmail.com>
2018-05-09 03:30:32 +0300

    Added relations methods.

--- app/Order.php
+++ app/Order.php
@@ -1,10 +1,26 @@
-<?php
-
-namespace App;
+<?php namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Order
+ * @package App
+ */
 class Order extends Model
 {
-    //
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function user()
+    {
+        return $this->hasOne('App\User');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function product()
+    {
+        return $this->hasOne('App\Product');
+    }
 }
